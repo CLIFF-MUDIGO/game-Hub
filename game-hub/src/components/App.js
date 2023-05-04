c3f338373b59522709054a602ad690ce746baa1b
cliff <cliffmudigo5@gmail.com>
2023-05-04 11:27:59 +0300

    fix nav and homepage

--- game-hub/src/components/App.js
+++ game-hub/src/components/App.js
@@ -3,17 +3,17 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './App.css';
-import { Link, Element } from 'react-scroll';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   const [games, setGames] = useState([]);
-
   useEffect(() => {
-    fetch('https://api.rawg.io/api/games?dates=2019-01-01,2021-01-01,2021-12-31&ordering=-added&page_size=6&key=49390ff6fa084eeebc9d464dedbd6685')
+    fetch(`https://api.rawg.io/api/games?key=72255820f866438197617e0ae9a982cc`)
       .then(response => response.json())
       .then(data => setGames(data.results));
   }, []);
-
   const settings = {
     dots: true,
     infinite: true,
@@ -23,7 +23,6 @@ function App() {
     autoplay: true,
     autoplaySpeed: 5000
   };
-
   return (
     <div className="app">
       <header>
@@ -31,50 +30,57 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="hero" smooth={true} duration={500}>
+              <a href="#home">
                 Home
-              </Link>
+              </a>
             </li>
             <li>
-              <Link to="games" smooth={true} duration={500}>
+              <a href="#games">
                 Games
-              </Link>
+              </a>
             </li>
             <li>
-              <Link to="about" smooth={true} duration={500}>
+              <a href="#about">
                 About
-              </Link>
+              </a>
             </li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
           </ul>
         </nav>
       </header>
       <main>
-        <Element name="hero">
-          <section className="hero">
-            <h2>Welcome to our game website</h2>
-            <p>Discover new games, read reviews, and join the gaming community.</p>
-            <button>Explore</button>
-          </section>
-        </Element>
-        <Element name="games">
-          <section className="slider-container">
+        <section className="home" id="home">
+          <h2>Welcome to our game website</h2>
+          <p>Discover new games, read reviews, and join the gaming community.</p>
+          <button>Explore</button>
+        </section>
+        <section className="slider-container" id="games">
+          {games.length > 0 ?
             <Slider {...settings}>
               {games.map(game => (
                 <div key={game.id} className="slider-item">
-                  <img src={game.background_image} alt={game.name} />
+                  <img src={game.background_image} alt={game.name} style={{ width: "200%" }} />
                   <h3>{game.name}</h3>
                 </div>
               ))}
             </Slider>
-          </section>
-        </Element>
-        <Element name="about">
-          <section className="about">
-            <h2>About us</h2>
-            <p>We are a team of passionate gamers who love to share our experience and knowledge with fellow gamers. Our goal is to create a community where gamers can connect, share and learn from one another. </p>
-          </section>
-        </Element>
+            :
+            <div>Loading...</div>
+          }
+        </section>
+        <section className="about" id="about">
+          <h2>About us</h2>
+          <p>We are a team of passionate gamers who love to share our experience and knowledge with fellow gamers. Our goal is to create a community where gamers can connect, share and learn from one another. </p>
+        </section>
+        <section className="contact" id="contact">
+          <h2>Contact us</h2>
+          <p>Follow us on Instagram:</p>
+          <a href="https://el._smash" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+        </section>
       </main>
       <footer>
         <p>© 2023 Game Website. All Rights Reserved.</p>
@@ -82,5 +88,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
